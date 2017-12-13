7677767ce0554fa0d62c8637e439e53f099e9d93
Pagi Pudl <pagipudl@gmail.com>
2017-12-13 16:24:45 +0100

    describers in dev

--- Utils/describers.py
+++ Utils/describers.py
@@ -22,27 +22,59 @@ class Material:
         return self.texture + ' ' + self.color + ' ' + self.name
 
 
-class Part:
-    def __init__(self, material, shape, age):
-        pass  # dodělat pro konkrétní item, tyhle hodnoty se mění - stáří, rozbitost..
+class State:
+    __slots__ = ["wear", "filth"]
+    wear_desc = ["factory new", "kinda new", "damaged", "totally broken"]  # TODO redo by somebody with language skills
+    filth_desc = ["clean as fuck", "clean", "in dirt covered", "in dirt and poop covered"]
+
+    def __init__(self, wear: int = 20, filth: int = 20):
+        self.wear = wear
+        self.filth = filth
 
     def describe(self, detail=0):
-        return self.material.describe(detail)
+        if detail == 0:
+            return ""
+        wr = State.wear_desc[int((self.wear / 100) * (len(State.wear_desc) - 1))]
+        fl = State.filth_desc[int((self.filth / 100) * (len(State.filth_desc) - 1))]
+        if detail == 1:
+            if self.wear < self.filth:
+                if self.wear < 20:
+                    return wr
+                return fl
+            else:
+                if self.filth < 20:
+                    return fl
+                return wr
+        if self.filth - self.wear > 50:
+            return wr + ", but " + fl
+        if self.wear - self.filth > 50:
+            return wr + ", but still " + fl
+        return wr + " " + fl
+
+
+class Part:
+    def __init__(self, material: Material, state: State, shape: str = "shapeless"):  # TODO implement shape etc..
+        self.material = material
+        self.state = state
+
+    def describe(self, detail=0):
+        return self.state.describe(detail) + " " + self.material.describe(detail)
 
 
 class Description:
-    def __init__(self, description: str, materials: tuple):
+    def __init__(self, description: str, parts: tuple):
         self.description = description
-        self.materials = materials
+        self.parts = parts
 
     def describe(self):
         text = self.description
-        for i in range(len(self.materials)):
-            text = text.replace('%' + str(i) + '%', self.materials[i].describe(1))
+        for i in range(len(self.parts)):
+            text = text.replace('%' + str(i) + '%', self.parts[i].describe(2))
         return text
 
 
 mat = Material("stone", "dark-red", "rough", "wet concrete", "tasteless", 50, 50, False, False)
-
-desc = Description("This is a %0%!!!", (mat,))
+st = State(5, 70)
+part = Part(mat, st)
+desc = Description("This is a %0%!!!", (part,))
 print(desc.describe())

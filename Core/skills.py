4ae4539f97b0c851987579197d9eb08af8a0c621
Pagi Pudl <pagipudl@gmail.com>
2017-12-13 16:44:47 +0100

    jožo mistakes

--- Core/skills.py
+++ Core/skills.py
@@ -1,22 +1,17 @@
-class AtributeHolder:
+class AttributeHolder:
     def __init__(self):
-        self.level=0
-        self.xp=0
-        self.xp_needed = lambda lvl: lvl*150
-
+        self.level = 0
+        self.xp = 0
+        self.xp_needed = lambda lvl: lvl * 150
 
 
 class Skills:
-    def __init__(self,all_skills):
-        self.all_skills={}
+    def __init__(self):
+        self.all_skills = {}
 
-    def set_skill(self,skill,level,xp):
+    def set_skill(self, skill, level, xp):
         if skill not in self.all_skills:
-            self.all_skills[skill]=AtributeHolder()
-        a=self.all_skills[skill]
-        a.level=level
-        a.xp=xp
-
-
-
-
+            self.all_skills[skill] = AttributeHolder()
+        a = self.all_skills[skill]
+        a.level = level
+        a.xp = xp
